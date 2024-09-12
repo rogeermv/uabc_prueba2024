e74f0f9cd1b83ff8848ab8ea34f297236c2877cf
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-11 21:41:09 -0700

    Update tt_um_uabc_prueba2024.v

--- src/tt_um_uabc_prueba2024.v
+++ src/tt_um_uabc_prueba2024.v
@@ -15,6 +15,12 @@ module tt_um_uabc_prueba2024 (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
 
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
